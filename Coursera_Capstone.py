df23b091b9def35703451956992c3ad35c4e94c9
Lilruskov <67399464+Lilruskov@users.noreply.github.com>
2020-06-25 04:00:48 +0200

    Delete Coursera_Capstone.py

--- Coursera_Capstone.py
+++
@@ -1,8 +0,0 @@
-# This notebook will be used in the context of the Coursera's Capstone project.
-
-# Importing librairies
-import pandas as pd
-import numpy as np
-
-# Code
-print("Hello Capstone Project Course")
